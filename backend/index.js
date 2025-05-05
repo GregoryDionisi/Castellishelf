ffbb202bc6612643d01a742179dbb6c94c94cf56
GregoryDionisi <gregoryd324@gmail.com>
2025-05-05 21:15:24 +0200

    Aggiunta backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require(`express`);
+const cors = require(`cors`);
+const bodyParser = require(`body-parser`);
+const {MongoClient} = require(`mongodb`);
+const { Portal } = require('@mui/material');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const connectToDatabase = async() => {
+    try{
+        const client = await MongoClient.connect(process.env.MONGO_URI);
+        console.log('Database is connected');
+        return client.db('castellishelf');
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+let database;
+
+const startServer = async() => {
+    database = await connectToDatabase();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+}
+
+startServer();
