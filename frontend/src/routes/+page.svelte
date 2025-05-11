b6d2812455ef9dc44b1010b21ed649e82b50c1cd
GregoryDionisi <gregoryd324@gmail.com>
2025-05-11 22:23:38 +0200

    Miglioramento interfaccia

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -492,7 +492,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
           </svg>
         </button>
-        <button class="p-2 map-control  transition-colors">
+        <button class="p-2 map-control transition-colors">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6"></path>
           </svg>
